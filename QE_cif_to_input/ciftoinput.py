c42aa5d71d08d197913f4b08ace79cfeb5164d27
AnoopANair <mailtoanoop71@gmail.com>
2020-11-26 16:46:22 +0000

    Merge branch 'main' of https://github.com/AnoopANair/QE_control into main

--- QE_cif_to_input/ciftoinput.py
+++ QE_cif_to_input/ciftoinput.py
@@ -7,14 +7,17 @@ alpha = math.pi/2
 beta = math.pi/2
 gamma = math.pi*(2/3)
 
-x1 = a
-y1 = 0
-z1 = 0
-x2 = b*math.cos(gamma)
-y2 = b*math.sin(gamma)
-z2 = 0
-x3 = c*math.cos(beta)
-y3 = c*(math.cos(alpha)- math.cos(gamma)*math.cos(beta))/math.sin(gamma)
-z3 = (b*b*math.sin(beta)*math.sin(beta) - y3*y3)**(0.5)
+def cellvectors(a,b,c,alpha,beta,gamma):
+    x1 = round(a,8)
+    y1 = round(0,8)
+    z1 = round(0,8)
+    x2 = round(b*math.cos(gamma),8)
+    y2 = round(b*math.sin(gamma),8)
+    z2 = round(0,8)
+    x3 = round(c*math.cos(beta),8)
+    y3 = round(c*(math.cos(alpha)- math.cos(gamma)*math.cos(beta))/math.sin(gamma),8)
+    z3 = round((b*b*math.sin(beta)*math.sin(beta) - y3*y3)**(0.5),8)
 
-print([[x1,y1,z1],[x2,y2,z2],[x3,y3,z3]])
+    return [[x1,y1,z1],[x2,y2,z2],[x3,y3,z3]]
+
+print(cellvectors(a,b,c,alpha,beta,gamma))
