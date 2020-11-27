ae84e2f51e5eb2fd25dfd76861d25ef19565f146
AnoopANair <mailtoanoop71@gmail.com>
2020-11-27 23:36:34 +0000

    cif

--- QE_ciffile_parse/ciffilparser.py
+++ QE_ciffile_parse/ciffilparser.py
@@ -7,9 +7,13 @@ def FileParser(File_name,search_word,splitter):
 
             line = re.findall(rf'{search_word}.*', line)
             if line:
-               line = line[0].split("{}".format(splitter))[1]
+               line = line[0].split("{}".format(splitter))
             if len(line) != 0:
-                print(line)
+                    line = [x for x in line if (x != '')]
+                    line = [x for x in line if (x != "'")]
+                    print(line)
+
+List_to_searched =["chemical_formula_sum","","space_group_IT_number","symmetry_space_group_name_Hall","symmetry_space_group_name_H-M","cell_angle_alpha","cell_angle_beta","cell_angle_gamma","cell_formula_units_Z","cell_length_a","cell_length_b","cell_length_c","cod_database_code"]               
 
 
-FileParser("Test.cif","_symmetry_space_group_name_H-M","'")
+FileParser("Test.cif","symmetry_space_group_name_Hall","'")
