d07e50a6ed493d79e54a17b0d05feb1737b807a2
AnoopANair <mailtoanoop71@gmail.com>
2020-11-29 10:56:21 +0000

    compile.py | code | normal compile version added to compile in google colab

--- QE_GpU_compile/compile.py
+++ QE_GpU_compile/compile.py
@@ -1,1 +1,22 @@
 import os
+
+
+"""
+Installing the Normal version of quantum ESPRESSO
+
+ #Clone the QE git
+ #Install the dependencies
+ #enter the q-e folder
+ #run the configure file
+ #make the pw file
+
+"""
+def compile_normal():
+    os.system("git clone https://github.com/QEF/q-e.git")  #Clone the QE git
+    os.system("apt-get install -y libfftw3–3 libfftw3-dev libfftw3-doc") #Install the dependencies
+    os.system("%cd q-e") #enter the q-e folder
+    os.system("DFLAGS = ‘-D__OPENMP -D__FFTW3 -D__MPI -D__SCALAPACK’ FFT_LIBS = ‘-lfftw3’ ./configure --enable-openmp") #run the configure file
+    os.system("make pw") #make the pw file
+
+def compile_gpu():
+    pass
